04cba2ef47e7a49de6b1d14d98474055b9a59025
Edwin29 <penaedwin299@gmail.com>
2018-09-26 09:46:30 -0600

    ajax1

---
+++ EjemploAjax/Controllers/AjaxController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EjemploAjax.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Operar( string nombre)
+        {
+            string n = nombre;
+            ViewBag.Resultado = n;
+            return View("Operar");
+        }
+    }
+}
---
+++ EjemploAjax/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EjemploAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult operacion (int n1,int n2, string opr )
+        {
+            /*Int32 t = numero * 5;
+            ViewBag.Resultado = t;*/
+             
+
+             
+
+            if (opr == "Potencia"){
+                double resultado = 0;
+                resultado = Math.Pow(n1, n2);
+                ViewBag.Resultado = resultado;
+            }
+            else if (opr =="Factorial")
+            {       int resultado = 1;
+                for (int i = 1; i <= n1; i++)
+                {
+                   
+                      resultado = resultado * i;
+                    
+                }
+                int resultado2 = 1;
+                for (int i = 1; i <= n2; i++)
+                {
+
+                    resultado2 = resultado2 * i;
+                    
+                }
+                ViewBag.Resultado = "El Factorial de " + n1 + " es : " + resultado + " y "+ "El Factorial de " + n2+ " es :" + resultado2;
+
+            }
+            else if (opr == "Mayor"){
+                if (n1 >n2){
+                    int res = 0;
+                    res=n1;
+                    ViewBag.Resultado ="El mayor es "+ res;
+                }
+                else {
+                    int res = 0;
+                     res=n2;
+                    ViewBag.Resultado = "El mayor es "+res;
+                }
+            }
+            else if (opr == "Menor"){
+                if (n1 <n2){
+                    int resul = 0;
+                    resul=n1;
+                    ViewBag.Resultado = "El menor es "+ resul;
+                }
+                else {
+                    int resul = 0;
+                    resul=n2;
+                    ViewBag.Resultado = "El menor es " + resul;
+                }
+            };
+
+
+
+            return PartialView("Resultado");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
